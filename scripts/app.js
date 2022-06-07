86bc1022baa8a84f8420466ef8da9bb8b0132bd1
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-06-07 14:50:03 -0700

    Song Playing + File Loading
    Reworked more of the original modules, and now they are used to open songs again! You can play them in the app by clicking on the song name in the library view.

--- scripts/app.js
+++ scripts/app.js
@@ -8,10 +8,19 @@ document.addEventListener("drop",async event => {
 
   if (!handles.length) return;
   const directory = handles.filter(handle => handle.kind === "directory")[0];
-  console.log(directory);
+  const tree = await fsa.dirtree(directory);
+  console.log(tree);
+  createTree({ tree });
+});
+
+const opener = document.querySelector("#opener");
+opener.addEventListener("click",() => {
+  createTree({ tree: library });
 });
 
 const main = document.querySelector("main");
+const player = document.querySelector("#player");
+const title = document.querySelector("#title");
 
 const library = await (await fetch("scripts/library.json")).json();
 
@@ -31,16 +40,28 @@ function createTree({ tree, parent = main } = {}){
     const opener = document.createElement("button");
 
     if (value){
-      details.open = true;
+      // details.open = true;
       summary.textContent = name;
       createTree({ tree: value, parent: content });
       details.append(summary,content);
       parent.append(details);
     } else {
       opener.textContent = name;
-      // opener.addEventListener("click",() => playAudio({ handle: entry }));
+      opener.addEventListener("click",() => playSong(entry.handle));
       content.append(opener);
       parent.append(content);
     }
   }
+}
+
+async function playSong(fileHandle){
+  if (fileHandle instanceof FileSystemFileHandle !== true) return;
+  const file = await fileHandle.getFile();
+  const { name, type } = file;
+  if (!type.includes("audio")) return;
+
+  const blob = window.URL.createObjectURL(file);
+  player.src = blob;
+  title.textContent = name;
+  player.paused ? player.play() : player.pause();
 }
