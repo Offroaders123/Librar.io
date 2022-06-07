71309ce75a748613fd5f9555a962bdb2839d69a8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-06-07 14:50:03 -0700

    Song Playing + File Loading
    Reworked more of the original modules, and now they are used to open songs again! You can play them in the app by clicking on the song name in the library view.

--- scripts/fsa.js
+++ scripts/fsa.js
@@ -4,4 +4,44 @@ export async function dataTransferHandles(dataTransfer){
   return handles;
 }
 
-export default { dataTransferHandles, [Symbol.toStringTag]: "fsa" };
+export async function readdir(directoryHandle,{ recursive = false } = {}){
+  const handles = [];
+  for await (const handle of directoryHandle.values()){
+    handles.push(handle.kind === "directory" && recursive === true ? await readdir(handle,{ recursive }) : handle);
+  }
+  return recursive === true ? handles.flat(Infinity) : handles;
+}
+
+export async function dirtree(directoryHandle){
+  const handles = await readdir(directoryHandle);
+
+  const entries = await Promise.all(handles.map(async entry => {
+    const { name, kind } = entry;
+    const value = kind === "directory" ? await dirtree(entry) : null;
+    const handle = kind === "file" ? entry : null;
+
+    return {
+      name,
+      ...value && ({ value }),
+      ...handle && ({ handle })
+    };
+  }));
+
+  return sortArray(entries);
+}
+
+function sortArray(array){
+  return array.sort((left,right) => {
+    left = formatKey(left.name);
+    right = formatKey(right.name);
+    if (left < right) return -1;
+    if (left > right) return 1;
+    return 0;
+  });
+}
+
+function formatKey(key){
+  return key.toLowerCase().replace(/^(The|A)\s/i,"");
+}
+
+export default { dataTransferHandles, readdir, dirtree, [Symbol.toStringTag]: "fsa" };
